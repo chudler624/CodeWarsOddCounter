61d44bee20de8a2783290b0d644ea00d8e60e44c
Casey Hudler <chudler624@gmail.com>
2022-08-30 13:12:15 -0400

    initial commit

---
+++ CodeWarsOddCounterVSC/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Pick a number");
+int userInput = int.Parse(Console.ReadLine());
+
+
+List<int> numbers = new List<int>();
+for (int i = userInput-1; i > 0; i--)
+{
+    if (i %2 == 1)
+    {
+        numbers.Add(i);
+    }
+}
+Console.WriteLine("number of odd numbers that are positive below your number");
+Console.WriteLine(numbers.Count);
+
+Console.WriteLine("these are the numbers that were added to the list");
+foreach (var item in numbers)
+{
+    Console.WriteLine(item);
+}
